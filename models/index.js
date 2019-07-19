f875fb520b6805301a3588a232afd810a0a76c16
snolan98k <snolan98k@gmail.com>
2019-07-19 14:21:57 -0400

    y

--- models/index.js
+++ models/index.js
@@ -1,15 +1,3 @@
-const app = express();
-
-const express = require('express')
-const path = require('path')
-var stripe = require('stripe')('sk_live_#');
-var bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
 'use strict';
 
 const fs = require('fs');
