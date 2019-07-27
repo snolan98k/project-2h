d96efac256c8250e3847107db3f40c09fea88a7c
Hicham <28849866+whitetoot@users.noreply.github.com>
2019-07-26 20:47:04 -0400

    db

--- routes/api-route.js
+++ routes/api-route.js
@@ -2,20 +2,20 @@
 const router = require("express").Router();
 const db = require("../models");
 
+module.exports = router;
 
 
 
-router.post("users", (req, res) => {
+router.post("/", (req, res) => {
     // use sequelize to store data into database
-    db.Users.create({
+    db.users.create({
         f_name: req.body.f_name,
+        l_name: req.body.l_name,
         email: req.body.email,
-        passphrase: req.body.passphrase,
-        l_name: req.body.l_name
+        passphrase: req.body.passphrase        
 
     }).then(function () {
-        res.redirect("/");
+        res.redirect("./home.html");
     });
 });
 
-module.exports = router;
