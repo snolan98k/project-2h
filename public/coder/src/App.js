13b66912417d66410cc16951efb936214bd6477f
snolan98k <snolan98k@gmail.com>
2019-09-05 15:45:41 -0400

    react

---
+++ public/coder/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { EventClaender } from "./component/EventClaender"
+
+function App() {
+  return (
+    <EventClaender />
+  )
+
+
+  export default App;
